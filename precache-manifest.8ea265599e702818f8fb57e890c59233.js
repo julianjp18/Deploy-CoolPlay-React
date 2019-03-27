76c802d592c16a82155445e5bd95a1e65c908d8d
Julian David Perez <julianperez.developer@gmail.com>
2019-03-27 16:22:59 -0500

    init repo

---
+++ precache-manifest.8ea265599e702818f8fb57e890c59233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc24b9c4b5f09c3ee4e2d5532d7b1814",
+    "url": "/Deploy-CoolPlay-React/static/media/1.bc24b9c4.jpg"
+  },
+  {
+    "revision": "ad645190df931c148b6c",
+    "url": "/Deploy-CoolPlay-React/static/js/runtime~main.5cc82204.js"
+  },
+  {
+    "revision": "3fe5c5127afb5ce04074",
+    "url": "/Deploy-CoolPlay-React/static/js/main.18a8dc6e.chunk.js"
+  },
+  {
+    "revision": "7a2c3a2317c74b09f9b0",
+    "url": "/Deploy-CoolPlay-React/static/js/2.b974b019.chunk.js"
+  },
+  {
+    "revision": "3fe5c5127afb5ce04074",
+    "url": "/Deploy-CoolPlay-React/static/css/main.98ba232a.chunk.css"
+  },
+  {
+    "revision": "7a2c3a2317c74b09f9b0",
+    "url": "/Deploy-CoolPlay-React/static/css/2.06a039b5.chunk.css"
+  },
+  {
+    "revision": "5fb0ca7380be7a148e4b70aab78bcd82",
+    "url": "/Deploy-CoolPlay-React/index.html"
+  }
+];
